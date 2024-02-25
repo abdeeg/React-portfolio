cb0c39a759f8c7c3cfe6b4dc2b7b58d5779e09b6
abdeeg <abdeegdahir@live.com>
2024-02-25 20:40:00 +0000

    Initializing Repo

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
